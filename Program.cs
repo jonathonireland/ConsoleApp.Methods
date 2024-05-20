788fe603c3cc86044188d6ae1870347b7c51dc6a
Jonathon Ireland <59446203+jonathonireland@users.noreply.github.com>
2024-05-20 12:50:37 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// See void methods -- complete a task without returning a value
+void PrintName()
+{
+    // Method Code
+    Console.WriteLine("Trevoir");
+}
+
+// value returning methods -  returns a value after an operation
+int GetFiveYearsAgo()
+{
+    int year = DateTime.Now.AddYears(-5).Year;
+    return year;
+}
+
+
+// methods parameters
+
+// methods with optional parameters
+
+// methods with nullable parameters
+
+/* Function Calls */
+PrintName();
+Console.WriteLine("Five Years Ago: " + GetFiveYearsAgo());
