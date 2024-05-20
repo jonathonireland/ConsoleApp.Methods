bbd7086d2c1aa52750b04294fbbdd2729cb58a1b
Jonathon Ireland <59446203+jonathonireland@users.noreply.github.com>
2024-05-20 14:07:50 -0400

    Tutorial 93 video

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿// See void methods -- complete a task without returning a value
 void PrintName()
 {
-    // Method Code
+ 
     Console.WriteLine("Trevoir");
 }
 
@@ -13,7 +13,21 @@ int GetFiveYearsAgo()
 }
 
 
-// methods parameters
+// methods with parameters - represent data being passed into a method
+void PrintNameWithParams(string name)
+{
+    Console.WriteLine("Your name is: "+name);
+}
+
+// value returning methods -  returns a value after an operation
+int GetYearDifferenceWithParams(int year)
+{
+    int yearDifference = DateTime.Now.Year - year;
+    return yearDifference;
+}
+
+
+
 
 // methods with optional parameters
 
@@ -22,3 +36,13 @@ int GetFiveYearsAgo()
 /* Function Calls */
 PrintName();
 Console.WriteLine("Five Years Ago: " + GetFiveYearsAgo());
+PrintNameWithParams("Dynamic Name");
+
+Console.WriteLine("Enter Your Name: ");
+string name1 = Console.ReadLine();
+PrintNameWithParams(name1);
+
+Console.WriteLine("Enter A Year: ");
+int pastYear = Convert.ToInt32(Console.ReadLine());
+int yearsAgo = GetYearDifferenceWithParams(pastYear);
+Console.WriteLine("This was " + yearsAgo + "years ago");
